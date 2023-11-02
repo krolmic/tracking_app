ed565223624bd5febf4cd2983fa91d8f5f30ca96
Michal Krol <hello@krolmic.dev>
2023-11-02 14:28:07 +0100

    feat 🚀: set viewPaddingTop to 0

--- lib/shared/constants/layout.dart
+++ lib/shared/constants/layout.dart
@@ -2,6 +2,6 @@
 const double verticalPaddingSmall = 5;
 const double verticalPaddingMedium = 10;
 const double verticalPaddingLarge = 15;
-const double viewPaddingTop = 20;
+const double viewPaddingTop = 0;
 const double viewPaddingBottom = 0;
 const double viewPaddingHorizontal = 25;
