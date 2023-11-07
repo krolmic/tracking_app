8f59c5ce3fba081de59fbd050a6e3f7a6af9d0fe
Michal Krol <hello@krolmic.dev>
2023-11-07 01:28:16 +0100

    chore 🔧:  AppCubit - move init to the top, move _configureLogging in init

--- lib/app/cubit/app_cubit.dart
+++ lib/app/cubit/app_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_datastore/amplify_datastore.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
@@ -20,6 +21,41 @@ class AppCubit extends Cubit<AppState> {
 
   final AmplifyClass _amplify;
 
+  Future<void> init() async {
+    emit(const AppState.loading());
+
+    await _configureLogging();
+
+    try {
+      await _configureAmplify();
+      emit(const AppState.success());
+    } catch (e, stackTrace) {
+      Fimber.e(
+        "Failed to initialise app's state.",
+        ex: e,
+        stacktrace: stackTrace,
+      );
+
+      emit(const AppState.error());
+    }
+  }
+
+  Future<void> _configureAmplify() async {
+    final datastore = AmplifyDataStore(
+      modelProvider: ModelProvider.instance,
+    );
+    final api = AmplifyAPI();
+
+    final auth = AmplifyAuthCognito();
+
+    await _amplify.addPlugins([
+      datastore,
+      api,
+      auth,
+    ]);
+    await _amplify.configure(amplifyconfig);
+  }
+
   Future<void> _configureLogging() async {
     if (kReleaseMode) {
       Fimber.plantTree(SentryTree());
@@ -33,32 +69,5 @@ class AppCubit extends Cubit<AppState> {
 
       Bloc.observer = LoggingBlocObserver();
     }
-  }
-
-  Future<void> init() async {
-    try {
-      emit(const AppState.loading());
-      await _configureLogging();
-      await _configureAmplify();
-      emit(const AppState.success());
-    } catch (e, stackTrace) {
-      // Fimber.e('Failed to initialise app's state.', ex: e, stacktrace: stackTrace);
-      emit(const AppState.error());
-    }
-  }
-
-  Future<void> _configureAmplify() async {
-    final datastorePlugin =
-        AmplifyDataStore(modelProvider: ModelProvider.instance);
-
-    final authPlugin = AmplifyAuthCognito();
-
-    await _amplify.addPlugins([
-      datastorePlugin,
-      authPlugin,
-    ]);
-    await _amplify.configure(amplifyconfig);
-
-    safePrint('Successfully configured');
   }
 }
