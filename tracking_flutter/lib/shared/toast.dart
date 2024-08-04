1ce12980568c9213e48bc4dc23227126491ddf2b
Michal Krol <hello@krolmic.dev>
2024-08-04 15:08:52 +0200

    feat 🚀: use animationDuration in showToast

--- tracking_flutter/lib/shared/toast.dart
+++ tracking_flutter/lib/shared/toast.dart
@@ -1,6 +1,7 @@
 import 'package:delightful_toast/delight_toast.dart';
 import 'package:delightful_toast/toast/components/toast_card.dart';
 import 'package:flutter/material.dart';
+import 'package:tracking_app/shared/theme/animation.dart';
 import 'package:tracking_app/shared/theme/colors.dart';
 
 void showToast(
@@ -9,6 +10,7 @@ void showToast(
   String message,
 ) {
   DelightToastBar(
+    animationDuration: animationDuration,
     builder: (context) => ToastCard(
       color: contentBackgroundColor,
       leading: Icon(icon),
