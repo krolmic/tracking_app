ed84172ea06bc78faf33668384a4d2b346f64ade
Michal Krol <hello@krolmic.dev>
2024-08-20 13:52:30 +0200

    feat 🚀: return 0 min for Duration.zero

--- tracking_flutter/lib/shared/extensions/duration.dart
+++ tracking_flutter/lib/shared/extensions/duration.dart
@@ -1,5 +1,9 @@
 extension DurationX on Duration {
   String toFormattedString() {
+    if (this == Duration.zero) {
+      return '0 min';
+    }
+
     final hours = inHours;
     final minutes = inMinutes.remainder(60);
     final minutesString = minutes != 0 ? '$minutes min' : '';
