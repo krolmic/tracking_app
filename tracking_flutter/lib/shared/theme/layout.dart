b6fd3ada8fd57c4a953d0114e1ba2bd25d6b5427
Michal Krol <hello@krolmic.dev>
2024-09-02 21:12:32 +0200

    feat 🚀: add max width to AppElevatedButton

--- tracking_flutter/lib/shared/theme/layout.dart
+++ tracking_flutter/lib/shared/theme/layout.dart
@@ -9,3 +9,6 @@ const double horizontalPaddingMedium = 10;
 const double horizontalPaddingLarge = 15;
 const double viewPaddingVertical = 15;
 const double viewPaddingHorizontal = 25;
+
+// View size constants
+const double maxViewWidth = 700;
