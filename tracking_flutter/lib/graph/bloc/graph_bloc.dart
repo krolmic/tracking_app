d1a69c3a58c7bee71bea849238c8153203cf89b9
Michal Krol <hello@krolmic.dev>
2024-10-15 19:09:45 +0200

    feat 🚀: keep target date in new state emit in _onTimeRangeModeChanged

--- tracking_flutter/lib/graph/bloc/graph_bloc.dart
+++ tracking_flutter/lib/graph/bloc/graph_bloc.dart
@@ -120,7 +120,6 @@ class GraphBloc extends Bloc<GraphEvent, GraphState> {
         settings: state.settings.copyWith(
           timeRangeMode: mode,
         ),
-        targetDate: const GraphTargetDateState.initial(),
       ),
     );
     await _loadMoods(emit);
